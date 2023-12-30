bbe3fc1e49bb9ec67674f6d973e75bd4cc233327
NeoThanatus <inushirodesu@gmail.com>
2023-12-29 22:19:47 -0400

    Deploy

---
+++ MyPortfolioPage/src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+        
+      </div>
+      <h1> NeoThanatus Portifolio Teste</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        É só um teste boy
+      </p>
+    </>
+  )
+}
+
+export default App
